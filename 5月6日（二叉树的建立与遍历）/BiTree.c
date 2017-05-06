630f6428dd43b809f79bca5934285b1864ab642f
X <P752101119@163.COM>
2017-05-06 23:29:05 +0800

    change coding

--- 5月6日（二叉树的建立与遍历）/BiTree.c
+++ 5月6日（二叉树的建立与遍历）/BiTree.c
@@ -1,1 +1,47 @@
-#include<stdio.h>#include<stdlib.h>typedef char etype;typedef struct tnode{	etype data;	struct tnode *lchild,*rchild;}tnode;void CreatBiTree(tnode **T){                  	       //因为要用malloc改变给出指向根节点的指针的值，改变了指针的值，所以函数形参这需要指针的指针来改变指针。    char c;		scanf("%c",&c);	if(' '==c){		*T=NULL;	}	else{		*T=(tnode *)malloc(sizeof(tnode));               //sizeof(tnode)和sizeof(tnode*)大小不一样，这里是要分配tnode数据类型占用的大小空间，将空间首地址给主函数中T！！！		(*T)->data=c;		CreatBiTree(&(*T)->lchild);		CreatBiTree(&(*T)->rchild);	}}void visit(etype data,int level){	printf("数据为:%c,它所在层数为:%d\n",data,level);}void PerOrderTraverse(tnode *T,int level){	if(T){		visit(T->data,level);		PerOrderTraverse(T->lchild,level+1);		PerOrderTraverse(T->rchild,level+1);	}	else{		return;	}}int main(){	int level=1;	tnode *T;	CreatBiTree(&T);	PerOrderTraverse(T,level);	return 0;}
+#include<stdio.h>
+#include<stdlib.h>
+typedef char etype;
+
+typedef struct tnode{
+	etype data;
+	struct tnode *lchild,*rchild;
+}tnode;
+
+void CreatBiTree(tnode **T){                  	       //因为要用malloc改变给出指向根节点的指针的值，改变了指针的值，所以函数形参这需要指针的指针来改变指针。
+    char c;
+	
+	scanf("%c",&c);
+	if(' '==c){
+		*T=NULL;
+	}
+	else{
+		*T=(tnode *)malloc(sizeof(tnode));               //sizeof(tnode)和sizeof(tnode*)大小不一样，这里是要分配tnode数据类型占用的大小空间，将空间首地址给主函数中T！！！
+		(*T)->data=c;
+		CreatBiTree(&(*T)->lchild);
+		CreatBiTree(&(*T)->rchild);
+	}
+}
+
+void visit(etype data,int level)
+{
+	printf("数据为:%c,它所在层数为:%d\n",data,level);
+}
+
+void PerOrderTraverse(tnode *T,int level){
+	if(T){
+		visit(T->data,level);
+		PerOrderTraverse(T->lchild,level+1);
+		PerOrderTraverse(T->rchild,level+1);
+	}
+	else{
+		return;
+	}
+}
+int main()
+{
+	int level=1;
+	tnode *T;
+	CreatBiTree(&T);
+	PerOrderTraverse(T,level);
+	return 0;
+}
--- 5月6日（二叉树的建立与遍历）/parent_chind.c
+++ 5月6日（二叉树的建立与遍历）/parent_chind.c
@@ -1,1 +1,23 @@
-//parent_child#define Msize 100typedef int etype;typedef struct cd   //孩子节点{	int chind;	strcut cd *next;}cd;typedef struct bt   //表头{	etype data;	int parent;	strcut bt *next;}bt;typedef struct tree{	bt tree[Msize];	int r,n;}
+//parent_child
+
+#define Msize 100
+typedef int etype;
+
+typedef struct cd   //孩子节点
+{
+	int chind;
+	strcut cd *next;
+}cd;
+
+typedef struct bt   //表头
+{
+	etype data;
+	int parent;
+	strcut bt *next;
+}bt;
+
+typedef struct tree
+{
+	bt tree[Msize];
+	int r,n;
+}
